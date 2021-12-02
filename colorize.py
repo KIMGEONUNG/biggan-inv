462c5904719d299a6f56811feb0166645eb1e2db
comar <saywooong@gmail.com>
2021-12-02 11:38:39 +0900

    feat: force condition

--- colorize.py
+++ colorize.py
@@ -12,6 +12,7 @@ from torchvision.utils import make_grid
 import torchvision.transforms as transforms
 from torchvision.transforms import ToPILImage, ToTensor
 from tqdm import tqdm
+from train_e import prepare_dataset, extract_sample
 
 
 def parse():
@@ -24,18 +25,29 @@ def parse():
     parser.add_argument('--num_row', type=int, default=8)
     parser.add_argument('--class_index', type=int, default=15)
     parser.add_argument('--size_batch', type=int, default=8)
+    parser.add_argument('--num_worker', default=8)
 
     # I/O
     parser.add_argument('--path_config', default='config.pickle')
-    parser.add_argument('--path_ckpt_g', default='./ckpts/encoder_f_16_finetune_v1/G_0016200.ckpt')
-    parser.add_argument('--path_ckpt_e', default='./ckpts/encoder_f_16_finetune_v1/E_0016200.ckpt')
+    parser.add_argument('--path_ckpt_g', default='./ckpts/test5/G_0018000.ckpt')
+    parser.add_argument('--path_ckpt_e', default='./ckpts/test5/E_0018000.ckpt')
     parser.add_argument('--path_output', default='./out_colorize')
+    parser.add_argument('--path_args', default='./ckpts/test5/args.pkl')
+    parser.add_argument('--path_imgnet_train', default='./imgnet/train')
+    parser.add_argument('--path_imgnet_val', default='./imgnet/val')
+
+    # Dataset
+    parser.add_argument('--iter_sample', default=4)
+
+    # User Input 
+    parser.add_argument('--index_target',
+            type=int, nargs='+', default=[11, 15])
+    parser.add_argument('--index_force',
+            type=int, default=14)
 
     parser.add_argument('--colorization_target', default='valid',
             choices=['valid', 'train']
             )
-    parser.add_argument('--path_dataset_encoder_train', default='./dataset_encoder/')
-    parser.add_argument('--path_dataset_encoder_valid', default='./dataset_encoder_val/')
 
     # Conversion
     parser.add_argument('--device', default='cuda:1')
@@ -85,9 +97,33 @@ def main(args):
 
     dev = args.device
 
+    prep = transforms.Compose([
+            ToTensor(),
+            transforms.Resize(256),
+            transforms.CenterCrop(256),
+            ])
+    dataset, dataset_val = prepare_dataset(
+            args.path_imgnet_train,
+            args.path_imgnet_val,
+            args.index_target,
+            prep=prep)
+    if args.colorization_target  == 'train':
+        print('Load train dataset')
+        dataset_target = dataset 
+    elif args.colorization_target  == 'valid':
+        print('Load valid dataset')
+        dataset_target = dataset_val
+    else:
+        raise Exception('Invalid colorization target')
+
+    dataloader = DataLoader(dataset_target, batch_size=args.size_batch,
+            shuffle=True, num_workers=args.num_worker, drop_last=True)
+
     # Load Configuratuion
     with open(args.path_config, 'rb') as f:
         config = pickle.load(f)
+    with open(args.path_args, 'rb') as f:
+        args_loaded = pickle.load(f)
 
     # Load Generator
     G = models.Generator(**config)
@@ -96,59 +132,42 @@ def main(args):
     G.eval()
 
     # Load Encoder
-    encoder = EncoderF_16(norm='instance').to(dev)
+    encoder = EncoderF_16(norm=args_loaded.norm_type)
     encoder.load_state_dict(torch.load(args.path_ckpt_e))
+    encoder.to(dev)
     encoder.eval()
 
-    # Latents
-    c = torch.ones(args.size_batch) * args.class_index
-    c = c.to(dev).long()
-
-    # Datasets
-    prep = transforms.Compose([
-                ToTensor(),
-                transforms.Resize(256),
-                transforms.CenterCrop(256),
-            ])
-
-    path_root = None
-    if args.colorization_target  == 'train':
-        path_root = args.path_dataset_encoder_train
-    elif args.colorization_target  == 'valid':
-        path_root = args.path_dataset_encoder_valid
-
-    dataset = ImageFolder(path_root, transform=prep)
-    dataloader  = DataLoader(dataset, batch_size=args.size_batch, shuffle=False,
-            num_workers=8, drop_last=True)
-    
     if not os.path.exists(args.path_output):
         os.mkdir(args.path_output)
 
     with torch.no_grad():
-        for i, (x, _) in enumerate(tqdm(dataloader)):
-            x_gt = x.clone().to(dev)
-            grid_gt = make_grid(x_gt, nrow=args.num_row)
+        for i, (x, c) in enumerate(tqdm(dataloader)):
+            x, c = x.to(dev), c.to(dev)
+            x_gray = transforms.Grayscale()(x)
 
             # Sample z
             z = torch.zeros((args.size_batch, G.dim_z)).to(dev)
             z.normal_(mean=0, std=0.8)
 
-            x = x.to(dev)
-            x = transforms.Grayscale()(x)
-            grid_gray = make_grid(x, nrow=args.num_row)
+            # Force Index
+            if args.index_force is not None:
+                c = ((c / c) * args.index_force).long()
 
-            f = encoder(x) # [batch, 1024, 16, 16]
+
+            f = encoder(x_gray) # [batch, 1024, 16, 16]
             output = G.forward_from(z, G.shared(c), args.num_layer, f)
             output = output.add(1).div(2)
-            grid_out = make_grid(output, nrow=args.num_row)
 
             # LAB
-            labs = fusion(x_gt.detach().cpu(), output.detach().cpu())
-            grid_lab = make_grid(labs, nrow=args.num_row).to(dev)
+            labs = fusion(x.detach().cpu(), output.detach().cpu())
 
+            # Save Result
+            grid_gt = make_grid(x, nrow=args.num_row)
+            grid_gray = make_grid(x_gray, nrow=args.num_row)
+            grid_out = make_grid(output, nrow=args.num_row)
+            grid_lab = make_grid(labs, nrow=args.num_row).to(dev)
             grid = torch.cat([grid_gt, grid_gray, grid_out, grid_lab], dim=-2)
             im = ToPILImage()(grid)
-
             im.save('./%s/%03d.jpg' % (args.path_output, i))
 
             if i > args.max_iter:
