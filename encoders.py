b65b633e20a3c6a6db511beeedda1e2a0733ebb1
comar <saywooong@gmail.com>
2021-12-06 17:16:09 +0900

    feat: good resencoder

--- encoders.py
+++ encoders.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import functools
 
 
 class ConvBlock(nn.Module):
@@ -131,6 +132,183 @@ class AdaConvBlock(nn.Module):
         if self.dropout is not None:
             x = self.dropout(x)
 
+        return x
+
+
+class ClassConditionNorm(nn.Module):
+    def __init__(self, 
+            output_size, 
+            input_size, 
+            which_linear=functools.partial(nn.Linear, bias=False), 
+            eps=1e-5, 
+            norm_style='bn'):
+        super().__init__()
+        self.output_size, self.input_size = output_size, input_size
+        # Prepare gain and bias layers
+        self.gain = which_linear(input_size, output_size)
+        self.bias = which_linear(input_size, output_size)
+        # epsilon to avoid dividing by 0
+        self.eps = eps
+        self.norm_style = norm_style 
+
+        self.register_buffer('stored_mean', torch.zeros(output_size))
+        self.register_buffer('stored_var', torch.ones(output_size)) 
+
+    def forward(self, x, y):
+        # Calculate class-conditional gains and biases
+        gain = (1 + self.gain(y)).view(y.size(0), -1, 1, 1)
+        bias = self.bias(y).view(y.size(0), -1, 1, 1)
+
+        if self.norm_style == 'bn':
+            out = F.batch_norm(x, self.stored_mean, self.stored_var, None, None,
+                              self.training, 0.1, self.eps)
+        elif self.norm_style == 'in':
+            out = F.instance_norm(x, self.stored_mean, self.stored_var, None, None,
+                              self.training, 0.1, self.eps)
+
+        return out * gain + bias
+
+
+class ResConvBlock(nn.Module):
+    def __init__(self, 
+            ch_in, 
+            ch_out, 
+            ch_c=128, 
+            is_down=False,
+            dropout=0.2,
+            activation='relu', 
+            pool='avg', 
+            norm='batch', 
+            **kwargs):
+        super().__init__()
+
+        self.is_down = is_down
+        self.has_condition = False
+
+        # Convolution
+        self.conv = nn.Conv2d(ch_in, ch_out, 
+                kernel_size=1, 
+                stride=1, 
+                padding=0)
+
+        self.conv_1 = nn.Conv2d(ch_in, ch_out, 
+                kernel_size=3, 
+                stride=1, 
+                padding=1)
+        self.conv_2 = nn.Conv2d(ch_out, ch_out, 
+                kernel_size=3, 
+                stride=1, 
+                padding=1)
+
+        # Normalization 
+        if norm == 'batch':
+            self.normalize_1 = nn.BatchNorm2d(ch_in)
+            self.normalize_2 = nn.BatchNorm2d(ch_out)
+        elif norm == 'instance':
+            self.normalize_1 = nn.InstanceNorm2d(ch_in)
+            self.normalize_2 = nn.InstanceNorm2d(ch_out)
+        elif norm == 'layer':
+            self.normalize_1 = nn.LayerNorm(kwargs['l_norm_shape_1'])
+            self.normalize_2 = nn.LayerNorm(kwargs['l_norm_shape_2'])
+        elif norm == 'adain':
+            self.has_condition = True 
+            self.normalize_1 = ClassConditionNorm(ch_in, ch_c, norm_style='in')
+            self.normalize_2 = ClassConditionNorm(ch_out, ch_c, norm_style='in')
+        elif norm == 'adabatch':
+            self.has_condition = True 
+            self.normalize_1 = ClassConditionNorm(ch_in, ch_c, norm_style='bn')
+            self.normalize_2 = ClassConditionNorm(ch_out, ch_c, norm_style='bn')
+        else:
+            raise Exception('Invalid Normalization')
+
+        # Nonlinearity 
+        self.activation = None
+        if activation == 'relu':
+            self.activation = lambda x: F.relu(x, True) 
+        elif activation == 'sigmoid':
+            self.activation = F.sigmoid
+        elif activation == 'lrelu':
+            slope = kwargs['l_slope']
+            self.activation = lambda x: F.leaky_relu(x, slope, True) 
+        else:
+            raise Exception('Invalid Nonlinearity')
+
+        # Pooling
+        self.pool = None
+        if pool == 'avg':
+            self.pool = lambda x: F.avg_pool2d(x, kernel_size=2)
+        elif pool == 'max':
+            self.pool = lambda x: F.max_pool2d(x, kernel_size=2)
+        elif pool == 'min':
+            self.pool = lambda x: F.min_pool2d(x, kernel_size=2)
+        else:
+            raise Exception('Invalid Pooling')
+
+        # Dropout
+        self.dropout = None
+        if dropout is not None:
+            self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, c=None):
+
+        # Residual Path
+        x_ = x
+
+        if self.has_condition:
+            x_ = self.normalize_1(x_, c)
+        else:
+            x_ = self.normalize_1(x_)
+        x_ = self.activation(x_)
+        x_ = self.conv_1(x_)
+
+        if self.is_down:
+            x_ = self.pool(x_)
+
+        if self.has_condition:
+            x_ = self.normalize_2(x_, c)
+        else:
+            x_ = self.normalize_2(x_)
+        x_ = self.activation(x_)
+        x_ = self.conv_2(x_)
+
+        # Main Path
+        if self.is_down:
+            x = self.pool(x)
+        x = self.conv(x)
+
+        # Merge
+        x = x + x_
+
+        if self.dropout is not None:
+            x = self.dropout(x)
+
+        return x
+
+
+class EncoderF_Res(nn.Module):
+    """
+    input feature: [batch, 3, 256, 256]
+    Target feature: [batch, 768, 16, 16]
+    """
+    def __init__(self, ch_in=1, ch_out=768, ch_unit=96, norm='batch'):
+        super().__init__()
+        # output is 96 x 256 x 256
+        self.res1 = ResConvBlock(ch_in, ch_unit * 1, is_down=False, norm=norm)
+        # output is 192 x 128 x 128 
+        self.res2 = ResConvBlock(ch_unit * 1, ch_unit * 2, is_down=True, norm=norm)
+        # output is  384 x 64 x 64 
+        self.res3 = ResConvBlock(ch_unit * 2, ch_unit * 4, is_down=True, norm=norm)
+        # output is  768 x 32 x 32 
+        self.res4 = ResConvBlock(ch_unit * 4, ch_unit * 8, is_down=True, norm=norm)
+        # output is  768 x 16 x 16 
+        self.res5 = ResConvBlock(ch_unit * 8, ch_unit * 8, is_down=True, norm=norm)
+
+    def forward(self, x, c=None):
+        x = self.res1(x, c)
+        x = self.res2(x, c)
+        x = self.res3(x, c)
+        x = self.res4(x, c)
+        x = self.res5(x, c)
         return x
 
 
@@ -444,7 +622,13 @@ class EncoderFZ_16(nn.Module):
 # index 6: ([batch, 96, 256, 256])
 # result: ([batch, 3 256, 256])
 if __name__ == '__main__':
-    model = EncoderFZ_16()
-    def count_parameters(model):
-        return sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(count_parameters(model))
+    # model = EncoderF_Res()
+    # def count_parameters(model):
+    #     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    # print(count_parameters(model))
+    from models.layers import GBlock
+    model = GBlock(6, 7)
+    print(model)
+
+
+    pass
