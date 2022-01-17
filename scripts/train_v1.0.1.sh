5e101acbb5ae440fa77ad54b11ba21f2a9d84009
comar <saywooong@gmail.com>
2022-01-17 17:35:02 +0900

    refact: ch task name

--- scripts/train_v1.0.1.sh
+++ scripts/train_v1.0.1.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -W ignore train.py \
         --path_log 'runs_c1000' \
         --vgg_target_layers 1 2 6 7 \
         --size_batch 60 \
-        --task_name fix_lpips \
+        --task_name 'lpips_low_feat' \
         --detail "Previous exp,train_v1.0.0, high level feature, like 23, 
         invoke unnatural results. For the more accurate color inference, we 
         need more low level feature, so change the vgg_target_layers" 
