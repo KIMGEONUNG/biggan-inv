2b2aa37549717dffd2d2bc234f90e6fc8aa39e1c
comar <saywooong@gmail.com>
2022-01-28 15:46:25 +0900

    feat: multi z

---
+++ scripts/multi_z.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source scripts/config
+
+set -e
+
+epoch=$(printf %03d $EPOCH)
+
+dir_target="$(pwd)/results/${NAME_TASK}_e${epoch}_zs"
+dir_gt='gt_valid'
+
+# INFERENCE
+python -W ignore app/colorize_zs.py \
+    --path_ckpt './ckpts/fix_lpips' \
+    --path_output $dir_target \
+    --epoch 11 \
+    --use_shuffle \
+    --use_ema 
