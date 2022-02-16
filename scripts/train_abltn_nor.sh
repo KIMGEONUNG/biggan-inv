7d3b349022c27cb8a00ad49da8749180db72ab62
comar <saywooong@gmail.com>
2022-02-16 11:44:16 +0900

    update: trivial

--- scripts/train_abltn_nor.sh
+++ scripts/train_abltn_nor.sh
@@ -5,10 +5,10 @@ CUDA_VISIBLE_DEVICES=0,1 python -W ignore train.py \
                     --vgg_target_layers 1 2 13 20 \
                     --size_batch 30 \
                     --num_epoch 10 \
-                    --norm_type ababatch \
+                    --norm_type adabatch \
                     --no_res \
                     --schedule_type mult \
                     --path_log 'runs_c100' \
-                    --task_name 'ablation_noc' \
+                    --task_name 'ablation_nor' \
                     --index_target {0..99} \
-                    --detail 'without residual path and class condition' 
+                    --detail 'without residual path' 
