7faa27be449be4eddb1ff9888f6ae5ce649d6770
comar <saywooong@gmail.com>
2022-04-06 15:41:16 +0900

    chconfig: gitignore, train script

--- train.sh
+++ train.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python -W ignore train.py \
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8 python -W ignore train.py \
-                    --use_enhance \
-                    --coef_enhance 1.2 \
+CUDA_VISIBLE_DEVICES=0 python -W ignore train.py \
                     --vgg_target_layers 1 2 13 20 \
-                    --size_batch 160 \
+                    --size_batch 16 \
                     --num_epoch 12 \
-                    --schedule_type mult \
-                    --dim_f 16 \
-                    --path_log 'runs_c1000' \
-                    --task_name 'best_full_batch_a' \
-                    --detail "full batch" 
+                    --path_log 'runs' \
+                    --task_name 'wip' \
+                    --detail 'wip' 
