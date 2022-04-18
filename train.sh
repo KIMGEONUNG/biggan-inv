1f6dbc8cef2b1a2b2409aa4d921fe5e1c1679db5
comar <saywooong@gmail.com>
2022-04-18 16:50:57 +0900

    update: trivial

--- train.sh
+++ train.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+source config.system.sh
+
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python -W ignore train.py \
-CUDA_VISIBLE_DEVICES=0 python -W ignore train.py \
+CUDA_VISIBLE_DEVICES=$GPUS python -W ignore train.py \
                     --vgg_target_layers 1 2 13 20 \
                     --loss_mse \
                     --loss_lpips \
                     --loss_adv \
-                    --size_batch 4 \
+                    --size_batch $SIZE_BATCH \
+                    --num_copy $NUM_COPY \
                     --index_target 42 88 93 96 110 \
                     --num_epoch 12 \
-                    --num_copy 4 \
                     --path_log 'runs' \
                     --task_name 'wip' \
                     --detail 'wip' 
