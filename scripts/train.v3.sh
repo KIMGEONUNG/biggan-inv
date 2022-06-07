d8f3b9a2c7130ed5c9541f0ef6557add1d1feb1b
comar <saywooong@gmail.com>
2022-06-07 09:16:55 +0900

    update: train script

---
+++ scripts/train.v3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source config.system.sh
+
+INDEX_TARGET=$(echo {0..99})
+LOSS_TARGETS="adv"
+NUM_EPOCH=5
+
+CUDA_VISIBLE_DEVICES=$GPUS python -W ignore train.py \
+                    --vgg_target_layers 1 2 13 20 \
+                    --loss_targets $LOSS_TARGETS \
+                    --size_batch $SIZE_BATCH \
+                    --interval_save_loss 10 \
+                    --interval_save_train 100 \
+                    --interval_save_test 100 \
+                    --dim_encoder_c 128 \
+                    --num_test_sample 15 \
+                    --index_target $INDEX_TARGET \
+                    --num_epoch $NUM_EPOCH \
+                    --path_log 'runs' \
+                    --task_name $(echo ${0##*/} | sed 's:.sh::' | sed 's:train.::') \
+                    --detail 'wip' 
