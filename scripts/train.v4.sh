ea5523f05abaaa82f5432a3502011d8962d54926
comar <saywooong@gmail.com>
2022-06-08 07:28:18 +0900

    update: trivial

---
+++ scripts/train.v4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source config.system.sh
+
+INDEX_TARGET=$(echo {0..999})
+LOSS_TARGETS="adv"
+NUM_EPOCH=12
+
+CUDA_VISIBLE_DEVICES=$GPUS python -W ignore train.py \
+                    --vgg_target_layers 1 2 13 20 \
+                    --no_save \
+                    --loss_targets $LOSS_TARGETS \
+                    --size_batch $SIZE_BATCH \
+                    --interval_save_loss 10 \
+                    --interval_save_train 1000 \
+                    --interval_save_test 2000 \
+                    --dim_encoder_c 128 \
+                    --chunk_size_z_e 0 \
+                    --coef_wip 0.02 \
+                    --num_test_sample 15 \
+                    --index_target $INDEX_TARGET \
+                    --num_epoch $NUM_EPOCH \
+                    --path_log 'runs' \
+                    --task_name $(echo ${0##*/} | sed 's:.sh::' | sed 's:train.::') \
+                    --detail 'wip' 
