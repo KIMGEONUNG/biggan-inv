3d74ec64234ec0e460d0af8a802dac243370fe7b
comar <saywooong@gmail.com>
2022-09-06 19:45:29 +0900

    update: trivial

---
+++ scripts/train.birds.save.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source config.system.sh
+
+INDEX_TARGET=$(cat resource/cls_group/birds10.txt | sed "s/,/ /g")
+LOSS_TARGETS="mse vgg_per adv"
+NUM_EPOCH=20
+
+CUDA_VISIBLE_DEVICES=$GPUS python -W ignore train.py \
+                    --vgg_target_layers 1 2 13 20 \
+                    --path_ckpts "/root/bigcolor" \
+                    --loss_targets $LOSS_TARGETS \
+                    --size_batch $SIZE_BATCH \
+                    --interval_save_loss 10 \
+                    --interval_save_train 100 \
+                    --interval_save_test 200 \
+                    --dim_encoder_c 128 \
+                    --chunk_size_z_e 0 \
+                    --coef_wip 0.02 \
+                    --num_test_sample 15 \
+                    --index_target $INDEX_TARGET \
+                    --num_epoch $NUM_EPOCH \
+                    --path_log 'runs' \
+                    --task_name $(echo ${0##*/} | sed 's:.sh::' | sed 's:train.::') \
+                    --detail 'wip' 
